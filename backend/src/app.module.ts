d4cfaa5d17257974bd22b42307647412a4597cd9
JordanSerafini <jordan@solution-logique.fr>
2024-07-22 10:53:40 +0200

    setup prisma pgsql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UtilisateursModule } from './utilisateurs/utilisateurs.module';
 import { UtilisateursService } from './utilisateurs/utilisateurs.service';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [UtilisateursModule],
+  imports: [UtilisateursModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService, UtilisateursService],
 })
